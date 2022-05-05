74f67056498028b187d8ec01278a751b53ca1ad6
Jake Garrison <95515322+jacoblgarrison@users.noreply.github.com>
2022-05-04 21:44:57 -0400

    Add files via upload
    Query code for the SQL database containing our weather information along with team data.

---
+++ NFL_Query_Code.sql
@@ -0,0 +1,35 @@
+CREATE TABLE NFL_Schedule_Data (
+	schedule_date DATE,
+	team_home TEXT,
+	team_away TEXT,
+	schedule_season INTEGER,
+	schedule_playoff BOOLEAN,
+	weather_temperature FLOAT,
+	weather_wind_mph FLOAT,
+	weather_humidity FLOAT,
+	weather_detail TEXT
+);
+
+CREATE TABLE Gambling_Data (
+	team_home TEXT,
+	score_home FLOAT,
+	score_away FLOAT,
+	team_away TEXT,
+	team_favorite_id TEXT,
+	spread_favorite FLOAT,
+	over_under FLOAT
+);
+
+CREATE TABLE Stadium_Data (
+	stadium TEXT,
+	stadium_neutral BOOLEAN,
+	team_home TEXT,
+	team_away TEXT
+);
+
+CREATE TABLE Weather_Data (
+	weather_temperature FLOAT,
+	weather_wind_mph FLOAT,
+	weather_humidity FLOAT,
+	weather_detail TEXT
+);
